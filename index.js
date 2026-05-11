397264475dc4e6c8e6de6cf6ebe1209c7da42cd5
unknown <andersonmaiast@gmail.com>
2026-05-11 19:09:56 -0300

    rota mensagem

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Backend funcionando!");
+});
+
+app.get("/mensagem", (req, res) => {
+  res.json({
+    texto: "Olá do backend!"
+  });
 });
 
 const PORT = process.env.PORT || 3000;
