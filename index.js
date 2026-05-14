022450b24a2fa05642bca89264bf427966947bae
unknown <andersonmaiast@gmail.com>
2026-05-13 23:29:30 -0300

    mais teste

--- index.js
+++ index.js
@@ -4,36 +4,48 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL);
-
-mongoose.connection.on("connected", () => {
-  console.log("Mongo conectado!");
-});
-
-mongoose.connection.on("error", (erro) => {
-  console.log("Erro Mongo:", erro);
-});
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Mongo conectado!");
+  })
+  .catch((erro) => {
+    console.log("Erro ao conectar Mongo:", erro);
+  });
 
 const Nota = mongoose.model("Nota", {
   notaFinal: Number
 });
 
+app.get("/", (req, res) => {
+  res.send("API funcionando!");
+});
 
 app.post("/notas", async (req, res) => {
 
-  const nota = new Nota({
-    notaFinal: req.body.notaFinal
-  });
+  try {
 
-  await nota.save();
+    const nota = new Nota({
+      notaFinal: req.body.notaFinal
+    });
 
-  res.json({
-    mensagem: "Salvo!"
-  });
-})
+    await nota.save();
+
+    res.json({
+      mensagem: "Salvo!"
+    });
+
+  } catch (erro) {
+
+    console.log("Erro ao salvar:", erro);
+
+    res.status(500).json({
+      erro: "Erro interno"
+    });
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 
